7956d574f80748b9bb2a741f89da7098d7ee1c2f
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-11 17:28:27 +0200

    feat(class/resource.js) resource class created

---
+++ class/resource.js
@@ -0,0 +1,41 @@
+export default class Resource {
+
+  /**
+   * @description Add one or multiple resources
+   * @param {Array|String} resource
+   */
+  add(resource) {
+
+  }
+
+  /**
+   * @description Get one or multiple resources
+   * @param {Array|String} resource
+   */
+  find(resource) {
+
+  }
+
+  /**
+   * @description Get all resources
+   */
+  findAll() {
+
+  }
+
+  /**
+   * @description Remove one or multiple resources
+   * @param {Array|String} resource
+   */
+  remove(resource) {
+
+  }
+
+  /**
+   * @description Remove all resources
+   */
+  removeAll() {
+
+  }
+
+}
