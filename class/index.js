a0ad1e63e85dc66fb6e5b8b7d3d5af7c0449389b
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-11 17:35:08 +0200

    feat(class/index.js) instantiate class with configuration

---
+++ class/index.js
@@ -0,0 +1,20 @@
+import Manager from './manager'
+import jsonOverride from 'json-override'
+
+let configuration
+
+export default {
+
+  /**
+   * @description Configure manager with object config
+   * @param config
+   */
+  configure: (config) => configuration = jsonOverride(configuration, config),
+
+  /**
+   * @description Instantiate class with configuration
+   * @return {Manager}
+   */
+  create: () => new Manager(configuration)
+
+}
