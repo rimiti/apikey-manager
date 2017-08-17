bdd7991a60a305ce9eee28693ebada7b0caa69a4
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-17 17:00:41 +0200

    feat(exceptions/key/found.js) key found exception created

---
+++ src/exceptions/key/found.js
@@ -0,0 +1,10 @@
+import Exception from '../base'
+
+export default class ExceptionKeyFound extends Exception {
+
+  constructor(message) {
+    super()
+    this.name = 'ExceptionKeyFound'
+    this.message = message
+  }
+}
