c11bb0111a6348a90da674377aa4717f4b792399
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-17 13:51:49 +0200

    feat(lib/manager.js) jwt default configuration created

--- src/lib/manager.js
+++ src/lib/manager.js
@@ -2,8 +2,20 @@ import manager from '../class'
 
 manager.configure({
     redis: {
-      host: '127.0.0.1',
+      host: "127.0.0.1",
       port: 6379
+    },
+    jwt: {
+      secret: "secret",
+      expiresIn: "",
+      notBefore: "",
+      audience: "",
+      issuer: "",
+      jwtid: "",
+      subject: "",
+      noTimestamp: "",
+      header: "",
+      keyid: ""
     }
   }
 )
