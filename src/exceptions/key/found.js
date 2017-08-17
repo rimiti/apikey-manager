d450312e120c69979c63d291f7a5e276c977a79f
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-17 17:11:43 +0200

    feat(exceptions/key/found.js) default message created

--- src/exceptions/key/found.js
+++ src/exceptions/key/found.js
@@ -5,6 +5,6 @@ export default class ExceptionKeyFound extends Exception {
   constructor(message) {
     super()
     this.name = 'ExceptionKeyFound'
-    this.message = message
+    this.message = message ? message : `Key already exist`
   }
 }
