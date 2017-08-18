363f0b37f32a292af2db903b9bdb36b443eddd47
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-18 18:06:14 +0200

    feat(exceptions/index.js) ExceptionResourceFound exception exported

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -1,7 +1,9 @@
 import ExceptionKeyFound from './key/found'
+import ExceptionResourceFound from './resources/found'
 import ExceptionResourceNotFound from './resources/not_found'
 
 export {
   ExceptionKeyFound,
+  ExceptionResourceFound,
   ExceptionResourceNotFound
 }
