cf59e62f17b4bb4d4ac3a01cc46ec17747ff7d69
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-18 10:46:12 +0200

    chore(test/index.js) generate API key with resource array parameter

--- test/index.js
+++ test/index.js
@@ -1,47 +1,55 @@
 import test from 'ava'
-import manager from '../lib/manager'
+import Manager from '../src/lib/manager'
+import jwt from 'jsonwebtoken'
+
+const manager = Manager.create()
 
 test('Generate API key with resource array parameter', t => {
-  manager.create(['resource1', 'resource2'])
+  return manager.create(['resource1', 'resource2'])
     .then(result => {
-      t.is(result, true)
+      jwt.verify(result.key, manager.jwt.secret)
+      t.true(Array.isArray(result.resources))
+      t.is(result.resources[0], 'resource1')
+      t.is(result.resources[1], 'resource2')
     })
 })
 
-test('Generate API key with string resource parameter', t => {
-  manager.create('resource')
-    .then(result => {
-      t.is(result, true)
-    })
-})
+// test('Generate API key with string resource parameter', t => {
+//   manager.create('resource')
+//     .then(result => {
+//       t.is(result, true)
+//     })
+// })
+//
+// test('Generate API key with array resource parameter and API key', t => {
+//   manager.create(['resource1', 'resource2'], 'myCustomeApiKey')
+//     .then(result => {
+//       t.is(result, true)
+//     })
+// })
+//
+// test('Generate API key with string resource parameter and API key', t => {
+//   manager.create('resource', 'myCustomeApiKey')
+//     .then(result => {
+//       t.is(result, true)
+//     })
+// })
+//
+// test('Delete resource from string parameter', t => {
+//   manager.deleteResource('resource', 'myCustomeApiKey')
+//     .then(result => {
+//       t.is(result, true)
+//     })
+// })
+//
+// test('Delete resources from array parameter', t => {
+//   manager.deleteResource(['resource1', 'resource2'], 'myCustomeApiKey')
+//     .then(result => {
+//       t.is(result, true)
+//     })
+// })
 
-test('Generate API key with array resource parameter and API key', t => {
-  manager.create(['resource1', 'resource2'], 'myCustomeApiKey')
-    .then(result => {
-      t.is(result, true)
-    })
-})
 
-test('Generate API key with string resource parameter and API key', t => {
-  manager.create('resource', 'myCustomeApiKey')
-    .then(result => {
-      t.is(result, true)
-    })
-})
-
-test('Delete resource from string parameter', t => {
-  manager.deleteResource('resource', 'myCustomeApiKey')
-    .then(result => {
-      t.is(result, true)
-    })
-})
-
-test('Delete resources from array parameter', t => {
-  manager.deleteResource(['resource1', 'resource2'], 'myCustomeApiKey')
-    .then(result => {
-      t.is(result, true)
-    })
-})
 
 
 
