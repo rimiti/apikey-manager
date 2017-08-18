a3c00720011abc18e948076c563261681155f4d6
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-18 17:25:09 +0200

    feat(exceptions/index.js) exception exported

--- src/exceptions/index.js
+++ src/exceptions/index.js
@@ -1,5 +1,7 @@
 import ExceptionKeyFound from './key/found'
+import ExceptionResourceNotFound from './resources/not_found'
 
 export {
-  ExceptionKeyFound
+  ExceptionKeyFound,
+  ExceptionResourceNotFound
 }
