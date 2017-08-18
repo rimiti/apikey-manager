cba33a247c4e5530f9c3154a64f90500e3a62c42
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-18 11:31:09 +0200

    chore(test/index.js) generate API key with resource array parameter (with API key set)

--- test/index.js
+++ test/index.js
@@ -4,23 +4,32 @@ import jwt from 'jsonwebtoken'
 
 const manager = Manager.create()
 
-test('Generate API key with resource array parameter', t => {
+test('Generate API key with resource array parameter (without API key set)', t => {
   return manager.create(['resource1', 'resource2'])
     .then(result => {
       jwt.verify(result.key, manager.jwt.secret)
-      t.true(Array.isArray(result.resources))
       t.is(result.resources[0], 'resource1')
       t.is(result.resources[1], 'resource2')
     })
 })
 
-// test('Generate API key with string resource parameter', t => {
-//   manager.create('resource')
-//     .then(result => {
-//       t.is(result, true)
-//     })
-// })
-//
+test('Generate API key with resource array parameter (with API key set)', t => {
+  return manager.create(['resource1', 'resource2'], 'S64Jp6yyfRK2nKZvnTv4wtNRcbt7VPXt5fMBqH7CaMXyPBRN')
+    .then(result => {
+      t.is(result.key, 'S64Jp6yyfRK2nKZvnTv4wtNRcbt7VPXt5fMBqH7CaMXyPBRN')
+      t.is(result.resources[0], 'resource1')
+      t.is(result.resources[1], 'resource2')
+    })
+})
+
+test('Generate API key with string resource parameter (without API key set)', t => {
+  return manager.create('resource')
+    .then(result => {
+      jwt.verify(result.key, manager.jwt.secret)
+      t.is(result.resources[0], 'resource')
+    })
+})
+
 // test('Generate API key with array resource parameter and API key', t => {
 //   manager.create(['resource1', 'resource2'], 'myCustomeApiKey')
 //     .then(result => {
