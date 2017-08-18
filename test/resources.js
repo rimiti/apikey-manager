f5943ba1d6efb2ce736e99023117c9fc56bce5c7
Dimitri Do Bairro <dimitri.dobairro@clicrdv.com>
2017-08-18 18:08:47 +0200

    chore(test/keys.js) tdd in progress for add resource method

--- test/resources.js
+++ test/resources.js
@@ -2,37 +2,40 @@ import test from 'ava'
 import Manager from '../src/lib/manager'
 
 const manager = Manager.create()
-let key = ''
+let access = ''
 
 test.before(async t => {
-  key = await manager.create('resource1')
+  access = await manager.create('resource1')
 })
 
 test('Add resources from array parameter', t => {
-
+  return manager.add(['resource1', 'resource2'], access.key)
+    .then(result => {
+      console.log(result)
+    })
 })
 
-test('Add resource from string parameter', t => {
-
-})
-
-test('Find resources from array parameter', t => {
-
-})
-
-test('Find resource from string parameter', t => {
-
-})
-
-test('Find all resources', t => {
-
-})
-
-test('Remove resources from array parameter', t => {
-
-})
-
-test('Remove resource from string parameter', t => {
-
-})
-
+// test('Add resource from string parameter', t => {
+//
+// })
+//
+// test('Find resources from array parameter', t => {
+//
+// })
+//
+// test('Find resource from string parameter', t => {
+//
+// })
+//
+// test('Find all resources', t => {
+//
+// })
+//
+// test('Remove resources from array parameter', t => {
+//
+// })
+//
+// test('Remove resource from string parameter', t => {
+//
+// })
+//
